f2ddffa36833b85a8ac617e43eb09e599a34d4b1
juanoubi6 <juan.manuel.oubina@gmail.com>
2020-05-22 11:18:16 -0300

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"net/http"
+	"runtime"
 	"sync"
+	"time"
 )
 
-//Expected result: executing HTTP calls with goroutines is faster than withouth them
+var cpuNumber = runtime.NumCPU()
+
+//Expected result: executing HTTP calls (IO operation) with goroutines is faster than without them as each one
+// can be executed in a separate goroutine and we don't have to wait a previous call to finish to schedule a new
+// one.
 func main() {
+	println("Number of CPUs: ", cpuNumber)
 	httpCallsWithGoroutines()
 	httpCallsWithoutGoroutines()
 }
 
-func httpCallsWithGoroutines(){
+func httpCallsWithGoroutines() {
 	fmt.Println("Starting calls with goroutines")
 	start := time.Now()
 	executeHttpCallsWithGoroutines()
@@ -21,19 +27,19 @@ func httpCallsWithGoroutines(){
 	fmt.Println("Finished calls with goroutines", finish.Sub(start))
 }
 
-func executeHttpCallsWithGoroutines(){
+func executeHttpCallsWithGoroutines() {
 	var waitForAll sync.WaitGroup
-	waitForAll.Add(3)
-	for j := 0; j <= 2; j++ {
-		go func(wait *sync.WaitGroup){
-			_, _ = http.Get("http://google.com/")
+	waitForAll.Add(cpuNumber)
+	for j := 0; j < cpuNumber; j++ {
+		go func(wait *sync.WaitGroup) {
+			doHttpCall()
 			wait.Done()
 		}(&waitForAll)
 	}
 	waitForAll.Wait()
 }
 
-func httpCallsWithoutGoroutines(){
+func httpCallsWithoutGoroutines() {
 	fmt.Println("Starting calls without goroutines")
 	start := time.Now()
 	executeHttpCallsWithoutGoroutines()
@@ -41,8 +47,12 @@ func httpCallsWithoutGoroutines(){
 	fmt.Println("Finished calls without goroutines", finish.Sub(start))
 }
 
-func executeHttpCallsWithoutGoroutines(){
-	_, _ = http.Get("http://google.com/")
-	_, _ = http.Get("http://google.com/")
+func executeHttpCallsWithoutGoroutines() {
+	for j := 0; j < cpuNumber; j++ {
+		doHttpCall()
+	}
+}
+
+func doHttpCall() {
 	_, _ = http.Get("http://google.com/")
 }
